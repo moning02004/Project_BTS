d66674815f1f079aa41f599fdbe4d44a18637f6d
Yu Jeonghoon <moning02004@naver.com>
2020-03-04 22:18:13 +0900

    [API]

--- bts-react/src/containers/post/PostDetail.js
+++ bts-react/src/containers/post/PostDetail.js
@@ -5,7 +5,6 @@ import Button from '@material-ui/core/Button';
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import { connect } from 'react-redux';
-import { queryAllByAttribute } from '@testing-library/react';
 
 
 const axios = require('axios');
